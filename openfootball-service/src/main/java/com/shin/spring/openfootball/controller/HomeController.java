49fe5912bf53215fea8ec789bf0594897c618e6b
Ho Minh Dat <hominhdatdn@gmail.com>
2015-09-11 00:37:32 +0700

    #1 Create Project: Run Basic Spring MVC

---
+++ openfootball-service/src/main/java/com/shin/spring/openfootball/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.shin.spring.openfootball.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by shin on 9/10/2015.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    @ResponseBody
+    public String ping() {
+        return "pong";
+    }
+}
